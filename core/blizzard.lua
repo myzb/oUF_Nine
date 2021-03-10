5da9d34324a1e62b17be2df2ca3e189b76821b9a
Matthias Y <myzb@users.noreply.github.com>
2021-03-10 19:02:36 +0100

    nine: register with omniCD

--- core/blizzard.lua
+++ core/blizzard.lua
@@ -1,6 +1,7 @@
-local _, ns = ...
+local addonName, ns = ...
 
 local config = ns.config
+local extras, events = CreateFrame('Frame'), {}
 
 -- ------------------------------------------------------------------------
 -- > BLIZZARD UI ADJUSTMENTS
@@ -34,4 +35,24 @@ if (config.blizzard.gametooltip and config.blizzard.gametooltip.move) then
 		self:SetPoint(pos.a1, pos.af, pos.a2, pos.x, pos.y)
 	end
 	hooksecurefunc('GameTooltip_SetDefaultAnchor', GameTooltip_Move)
+end
+
+-- OmniCD registration
+function events:ADDON_LOADED(name)
+	if name == addonName or name == 'OmniCD' then
+		local func = OmniCD and OmniCD.AddUnitFrameData
+		if func then
+			func(addonName, addonName..'Raid1UnitButton', 'unit', 1)
+		end
+	end
+end
+
+-- event registration
+do
+	for k, _ in pairs(events) do
+		extras:RegisterEvent(k)
+	end
+	extras:SetScript('OnEvent', function(element, event, ...)
+		events[event](element, ...)
+	end)
 end
